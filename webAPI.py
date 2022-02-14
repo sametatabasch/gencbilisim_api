36ad201e9b40bd8324f9be74c9366eb6e7d80b51
sametatabasch <sametatabasch@gmail.com>
2022-02-14 03:30:54 +0300

    fix unable to open database file

--- webAPI.py
+++ webAPI.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request, jsonify
 from Data import data
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -14,7 +15,7 @@ def hello_world():  # put application's code here
 @app.route('/relayStatus')
 def role_status():
     db = connect_sqlite3()
-    c= db.cursor()
+    c = db.cursor()
     try:
         relays = c.execute("SELECT * FROM relays;").fetchall()
         print(data)
@@ -27,16 +28,21 @@ def role_status():
 @app.route('/changeRelayStatus')
 def change_relay_status():
     return request.args
+
+
 """
     change factory for novel syntax of json api
 """
+
+
 def dict_factory(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
     return d
 
-def connect_sqlite3(db_file="webAPI.db"):
+
+def connect_sqlite3(db_file=os.path.dirname(__file__) + "webAPI.db"):
     conn = None
     try:
         conn = sqlite3.connect(db_file)
