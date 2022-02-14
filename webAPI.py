d2e28a842af4e12f753796e21527df223eb9881f
sametatabasch <sametatabasch@gmail.com>
2022-02-14 18:06:19 +0300

    add CORS support

--- webAPI.py
+++ webAPI.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask import request, jsonify
 import sqlite3
 import os
-
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def hello_world():  # put application's code here
