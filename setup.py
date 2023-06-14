30549ef05915ec3a2a4e11814882c5c79f8d7c8b
sametatabasch <sametatabasch@gmail.com>
2023-06-14 13:24:06 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='gencbilisim_api',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='sametatabasch',
+    author_email='sametatabasch@gmail.com',
+    description='',
+    install_requires=[
+        'Flask',
+        'flask-cors'
+    ],
+)
