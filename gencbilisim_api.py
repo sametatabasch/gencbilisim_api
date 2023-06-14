fd979f88b1bc99d37b23ed8510b05ad8b5f56fba
sametatabasch <sametatabasch@gmail.com>
2023-06-15 00:13:24 +0300

    flask_jwt_extended sınıfı kullanılarak kullanıcı girişi ve access token oluşturma işlermleri yapıldı

--- gencbilisim_api.py
+++ gencbilisim_api.py
@@ -1,9 +1,12 @@
+import os
+import sqlite3
+
+from dotenv import load_dotenv
 from flask import Flask
 from flask import request, jsonify
-import sqlite3
-import os
 from flask_cors import CORS
-from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+from .models.Database import Database
 
 app = Flask(__name__)
 
@@ -14,20 +17,63 @@ for key, value in os.environ.items():
 
 CORS(app)
 
+jwt = JWTManager(app)
+db = Database()
+
+
+# Kullanıcı verilerini veritabanından sorgulama
+def get_user(username):
+    db.connect()
+    db.cursor.execute('SELECT * FROM users WHERE username = ? AND active = ?', (username, 1,))
+    return db.cursor.fetchone()
+
+
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    if not username or not password:
+        return jsonify({'message': 'Geçersiz kullanıcı adı veya şifre'}), 400
+
+    user = get_user(username)
+
+    if not user or user['password'] != password:
+        return jsonify({'message': 'Geçersiz kullanıcı adı veya şifre'}), 401
+
+    try:
+        access_token = create_access_token(identity=user)
+        return jsonify({'access_token': access_token}), 200
+    except Exception as e:
+        return jsonify({'message': f'Hata oluştu: {str(e)}'}), 500
+
+
+# Korumalı bir rotaya örnek
+@app.route('/protected', methods=['POST'])
+@jwt_required()
+def protected():
+    try:
+        current_user = get_jwt_identity()
+        return jsonify({'message': f'Hoş geldin, {current_user["name"]}! Bu korumalı veriye erişebilirsin.'}), 200
+    except Exception as e:
+        return jsonify({'message': f'Hata oluştu: {str(e)}'}), 500
+
+
 @app.route('/relayStatus')
 def role_status():
-    db = connect_sqlite3()
-    c = db.cursor()
+    db.connect()
     relays = None
     try:
-        relays = c.execute("SELECT * FROM relays;").fetchall()
+        relays = Database.cursor.execute("SELECT * FROM relays;").fetchall()
+        db.disconnect()
     except sqlite3.Error as e:
         print(e)
+        db.disconnect()
     return jsonify(relays)
 
 
@@ -45,39 +91,34 @@ def change_relay_status():
         }
     ]
     try:
-        db = connect_sqlite3()
-        c = db.cursor()
+        db.connect()
         for relay in relays:
-            c.execute('''UPDATE relays SET status=? WHERE name=?''', (relay['status'], relay['name']))
+            db.cursor.execute('''UPDATE relays SET status=? WHERE name=?''', (relay['status'], relay['name']))
 
-        db.commit()
+        db.connection.commit()
+        db.disconnect()
         return "Success!"
     except Exception as e:
+        db.disconnect()
         return e
 
 
-def dict_factory(cursor, row):
-    """
-        change factory for novel syntax of json api
-        database data return dict instead of tuple
-    """
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
+@app.errorhandler(403)
+def forbidden(e):
+    return jsonify({
+        "message": "Forbidden",
+        "error": str(e),
+        "data": None
+    }), 403
 
 
-def connect_sqlite3(db_file=app.config['DATABASE_PATH']):
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        c = conn.cursor()
-        conn.row_factory = dict_factory
-        return conn
-    except sqlite3.Error as e:
-        print(e)
-
-    return conn
+@app.errorhandler(404)
+def forbidden(e):
+    return jsonify({
+        "message": "Endpoint Not Found",
+        "error": str(e),
+        "data": None
+    }), 404
 
 
 if __name__ == '__main__':
