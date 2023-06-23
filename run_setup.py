f50650731ecbc03dba9db1954cd23c9718499ab5
sametatabasch <sametatabasch@gmail.com>
2023-06-23 03:37:17 +0300

    ATTENDANCE_DATABASE veri tabanı eklendi

--- run_setup.py
+++ run_setup.py
@@ -2,11 +2,13 @@ from dotenv import load_dotenv
 import sqlite3
 import os
 import secrets
+import json
 from werkzeug.security import generate_password_hash
 
 load_dotenv()
 
-DATABASE_PATH = os.environ.get('DATABASE_PATH') or 'mydatabase.db'
+DATABASE_PATH = os.environ.get('DATABASE_PATH')
+ATTENDANCE_DATABASE_PATH = os.environ.get('ATTENDANCE_DATABASE_PATH')
 
 
 def create_tables():
@@ -33,10 +35,45 @@ def create_tables():
             UNIQUE(name)
         );
     ''')
+    conn.commit()
+    conn.close()
 
+    conn = sqlite3.connect(ATTENDANCE_DATABASE_PATH)
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS students (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            last_name TEXT,
+            student_id TEXT,
+            card_id TEXT,
+            UNIQUE(student_id),
+            UNIQUE(card_id)
+    );
+    ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS lecturer (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            last_name TEXT,
+            card_id TEXT,
+            schedule TEXT,
+            UNIQUE(card_id)
+        );
+        ''')
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS lessons(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                code TEXT,
+                attendance TEXT,
+                UNIQUE(code)
+            );
+            ''')
     conn.commit()
     conn.close()
     print("Veri tabanı tabloları oluşturuldu")
+
 
 def insert_initial_data():
     conn = sqlite3.connect(DATABASE_PATH)
@@ -53,7 +90,89 @@ def insert_initial_data():
 
     conn.commit()
     conn.close()
+
+    conn = sqlite3.connect(ATTENDANCE_DATABASE_PATH)
+    cursor = conn.cursor()
+    cursor.executemany('''
+            INSERT OR IGNORE INTO students (name, last_name, student_id, card_id)
+            VALUES (?, ?, ?, ?)
+        ''', [
+        ('Samet', 'ATABAŞ', '090606043', '93bdd50b'),
+        ('Kart', ' Bir', '090606044', '3413fc51'),
+        ('Kart', ' İki', '090606045', '2aca190b'),
+        ('Diş', ' KTU', '090606046', 'd226d935'),
+    ])
+
+    cursor.execute('''
+        INSERT OR IGNORE INTO lecturer (name, last_name, card_id, schedule)
+        VALUES (?, ?, ?, ?)
+        ''', ("Samet", "ATABAŞ", "d56ef659",
+              json.dumps({
+                  0: {
+                      "BILP-113": [[10, 0], [15, 0]],
+                      "BILP-114": [[15, 0], [17, 0]]
+
+                  }, 1: {
+                      "BILP-2": [[12, 0], [20, 29]]
+                  }, 2: {
+                      "BILP-3": [[12, 0], [17, 0]]
+                  }, 3: {
+                      "GZT-105": [[9, 0], [16, 0]]
+
+                  }, 4: {
+                      "BILP-201": [[8, 0], [10, 00]],
+                      "BILP-207": [[10, 0], [12, 00]],
+                      "BILP-107": [[13, 0], [17, 00]],
+                  },
+                  5: {
+
+                  },
+                  6: {
+
+                  }})
+              )
+                   )
+
+    cursor.executemany('''
+        INSERT OR IGNORE INTO lessons (name, code, attendance)
+        VALUES (?, ?, ?)
+        ''', [
+        ('Mathematics', 'MATH101', json.dumps(
+            {
+                1: [
+                    {"card_id": "93bdd50b", "student_id": "090606043"},
+                    {"card_id": "3413fc51", "student_id": "090606044"}
+                ],
+                2: [
+                    {"card_id": "93bdd50b", "student_id": "090606043"}
+                ]
+            }
+        )),
+        ('Physics', 'PHYS201', json.dumps(
+            {
+                1: [
+                    {"card_id": "93bdd50b", "student_id": "090606043"},
+                    {"card_id": "3413fc51", "student_id": "090606044"}
+                ],
+                2: [
+                    {"card_id": "93bdd50b", "student_id": "090606043"}
+                ]
+            }
+        )),
+        ('Chemistry', 'CHEM301', json.dumps(
+            {
+                1: [
+                    {"card_id": "93bdd50b", "student_id": "090606043"},
+                    {"card_id": "3413fc51", "student_id": "090606044"}
+                ],
+                2: [
+                    {"card_id": "93bdd50b", "student_id": "090606043"}
+                ]
+            }
+        ))
+    ])
     print("Tablolara örnek ilk veriler eklendi")
+
 
 def set_secret_key():
     # .env dosyasını oku ve mevcut değerleri al
